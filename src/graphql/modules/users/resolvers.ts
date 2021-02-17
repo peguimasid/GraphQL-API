6696d7284bf6cb0753f7d3d05fb996477d31269a
Guilhermo Masid <guilhermomasid@gmail.com>
2021-02-17 11:54:31 -0300

    real time/websockets com GraphQL Subscriptions

--- src/graphql/modules/users/resolvers.ts
+++ src/graphql/modules/users/resolvers.ts
@@ -1,5 +1,6 @@
 import User from '../../../models/User'
 import { ObjectId } from 'mongoose'
+import { USER_ADDED } from './channels'
 
 interface IUser {
   firstName: string
@@ -22,8 +23,21 @@ export default {
     findUserBy: (_: unknown, { id }: IUserData) => User.findById(id)
   },
   Mutation: {
-    createUser: (_: unknown, { data }: IUserData) => User.create(data),
+    createUser: async (_: unknown, { data }: IUserData, { pubsub }: any) => {
+      const user = await User.create(data)
+
+      pubsub.publish(USER_ADDED, {
+        userAdded: user
+      })
+
+      return user
+    },
     updateUser: (_: unknown, { id, data }: IUserData) => User.findByIdAndUpdate(id, data, { new: true }),
     deleteUser: async (_: unknown, { id }: IUserData) => !!(await User.findByIdAndDelete(id))
+  },
+  Subscription: {
+    userAdded: {
+      subscribe: (obj: any, args: any, { pubsub }: any) => pubsub.asyncIterator(USER_ADDED)
+    }
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer, PubSub } from 'apollo-server'
 import mongoose from 'mongoose'
 
 function startServer({ typeDefs , resolvers }: any) {
@@ -7,7 +7,8 @@ function startServer({ typeDefs , resolvers }: any) {
     useUnifiedTopology: true,
   })
 
-  const server = new ApolloServer({ typeDefs, resolvers })
+  const pubsub = new PubSub()
+  const server = new ApolloServer({ typeDefs, resolvers, context: { pubsub } })
 
   server.listen().then(({ url }) => console.log(`🍄 Server started at ${url}`))
 }
